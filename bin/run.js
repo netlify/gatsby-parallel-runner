785aac4626801544ea0f9a198a300ee55211275a
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2020-02-05 12:57:57 -0800

    Fix error in run script

--- bin/run.js
+++ bin/run.js
@@ -16,16 +16,15 @@ if (!process.env.GOOGLE_APPLICATION_CREDENTIALS.match(/\.json$/)) {
   const credentialsFile = '/tmp/credentials.json'
   let credentials = null
   try {
-    credentials = atob(process.env.GOOGLE_APPLICATION_CREDENTIALS)
+    let buff = new Buffer(process.env.GOOGLE_APPLICATION_CREDENTIALS, 'base64')
+    credentials = buff.toString('ascii')
   } catch(err) {
-    console.error("GOOGLE_APPLICATION_CREDENTIALS must either be a path to a .json file or base 64 encoded json credentials")
+    console.error("GOOGLE_APPLICATION_CREDENTIALS must either be a path to a .json file or base 64 encoded json credentials", err)
     process.exit(1)
   }
   writeFileSync(credentialsFile, credentials)
   process.env.GOOGLE_APPLICATION_CREDENTIALS = credentialsFile
 }
-
-const data = readFile
 
 if (process.argv.length === 3 && process.argv[2] === 'deploy') {
   console.log("Deploying Cloud Worker")
